3dbeeab379be1bedd632f50f6a3ff1fc022e3089
Nischal-2006 <nischal1.815421@bumc.tu.edu.np>
2025-05-21 22:45:22 +0545

    added some styles

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ const songs = [
             {
                 name: "Aalas Ka Pedh - The Local Train",
                 path:"musics/The_Local_Train_-_Aalas_Ka_Pedh_-_Choo_Lo__Official_Audio_(128k).mp3" },
+                { name: "Gurasai-Fulyo Banaima_1974_AD", path: "musics/Gurasai_Fulyo_Banaima__गुराँसै_फुल्यो_वनैमा____1974_AD___Lyrical_Video_With_Guitar_Chords(128k).mp3" },
+    
+                { name: "Mercedez-Benz-CowWeb", path: "musics/Mercedes_Benz_-_Cobweb___Tuborg_Open_Sessions(128k).mp3" },
+                { name: "Saiyaan - Kailash Kher", 
+                    path: "musics/Saiyyan_-_Kailash_Kher__Paresh_Kamath__Naresh_Kamath___Jhoomo_Re(128k).mp3" },
+                     {
+                        name:" Sajni - Arijit Singh, Ram Sampath",
+                        path: "musics/Sajni__Song___Arijit_Singh,_Ram_Sampath___Laapataa_Ladies____Aamir_Khan_Productions(256k).mp3" },
+                        {
+                            name: "Aalas Ka Pedh - The Local Train",
+                            path:"musics/The_Local_Train_-_Aalas_Ka_Pedh_-_Choo_Lo__Official_Audio_(128k).mp3" },
+            
 
   ];
   
